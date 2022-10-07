b01cc806bfbc27aaafcfcc75e7e14325f6bf630c
joemags-apps <28527609+Jmunapo@users.noreply.github.com>
2022-10-08 00:01:07 +0200

    deploy

---
+++ _nuxt/51bc180.js
@@ -0,0 +1,2 @@
+/*! For license information please see LICENSES */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{347:function(e,n,r){"use strict";r.r(n);var o=r(336);r.d(n,"default",(function(){return o.a}));o.a.registerVersion("firebase","9.10.0","app-compat")}}]);
